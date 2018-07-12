cabe5543fffce03c6c0c39bb4aa841ecea9b6710
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-07-12 13:25:05 -0600

    created migration file

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,12 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/dbname-dev'
+    connection: 'postgres://localhost/user-dev'
   },
-  test: {},
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/user-test'
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
