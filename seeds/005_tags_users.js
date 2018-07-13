370f25e50f36638bb59ddf4ab8884e99b73083da
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-07-13 10:49:39 -0600

    all seeds and all migrations built

--- seeds/005_tags_users.js
+++ seeds/005_tags_users.js
@@ -7,14 +7,14 @@ exports.seed = function(knex, Promise) {
           id: 1,
           user_id: 2,
           tag_id: 1,
-          // created_at: '2018-02-05T14:26:16.000Z',
-          // updated_at: '2018-02-05T14:26:16.000Z'
+          created_at: '2018-02-05T14:26:16.000Z',
+          updated_at: '2018-02-05T14:26:16.000Z'
         }, {
           id: 2,
           user_id: 1,
           tag_id: 1,
-          // created_at: '2018-02-05T14:26:16.000Z',
-          // updated_at: '2018-02-05T14:26:16.000Z'
+          created_at: '2018-02-05T14:26:16.000Z',
+          updated_at: '2018-02-05T14:26:16.000Z'
         }
       ])])
   })
